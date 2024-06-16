438bcc5d41656075393e0b101933839185a5f770
Damian Wileński <wilenskid.dev@gmail.com>
2024-06-16 17:00:18 +0200

    commented out the worker select;

--- module/editor/src/component/sidebar.jsx
+++ module/editor/src/component/sidebar.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import css from '$editor/component/style/sidebar.module.css';
 import { SidebarMenu } from '$editor/component/sidebar-menu';
 import { Versions } from '$editor/component/versions';
-import { WorkerSelect } from '$editor/container/grammar-select';
+// import { WorkerSelect } from '$editor/container/grammar-select';
 import { ColorModeSwitch } from '$editor/container/color-mode-switch';
 
 export const Sidebar = () => (
   <div className={css.sidebar} data-testid="ti-sidebar">
-    <WorkerSelect />
+    {/* <WorkerSelect /> */}
     <SidebarMenu />
     <div style={{ flex: 1 }}></div>
     <ColorModeSwitch />
